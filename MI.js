273613db33a47a980f3fcb599997dfc1f491221e
Lobst3rG3off <erip.pasei@gmail.com>
2020-02-11 15:37:36 -0500

    created basic for loop function

--- MI.js
+++ MI.js
@@ -4,4 +4,17 @@ const map = [
     [0,0,1,0,0],
     [0,0,0,1,1],
 ]
+//part A
 
+
+
+
+
+const arrayReader= function(array)  {
+    for(let i = 0; i< array.length; i++) {
+        console.log('It Works!');
+    }
+
+}
+
+arrayReader(map);
