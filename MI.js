06d6cfd419dc26b9a909d19694702973ed804eb5
Lobst3rG3off <erip.pasei@gmail.com>
2020-02-11 16:04:37 -0500

    cleaned up part 1

--- MI.js
+++ MI.js
@@ -20,32 +20,7 @@ const arrayReader= function(array)  {
             }
             else
                 console.log('Thats a zero')
-
-
         }
     }
-    // const arrayReader = function (array) {
-    //     for (let i = 0; i < array.length; i++) {
-    //         console.log(array[i]);
-    //     }
-
-
-    // }
-
-}
-
-
-// for (i = 0; i < numbers.length; i++) {
-//     for (j = 0; j < numbers[i].length; j++) {
-//         if (numbers[i][j] % 2 === 0) {
-//             numbers[i][j] = "even";
-//         }
-//         else
-//             numbers[i][j] = "odd";
-
-
-//     }
-
-// }
 
 arrayReader(islandArray);
