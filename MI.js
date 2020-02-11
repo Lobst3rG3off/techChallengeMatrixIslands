cd07d86ee830e733ce6bbfe252794b122216c848
Lobst3rG3off <erip.pasei@gmail.com>
2020-02-11 16:50:12 -0500

    cleaned up brackets

--- MI.js
+++ MI.js
@@ -16,11 +16,33 @@ const arrayReader= function(array)  {
         for (j = 0; j < array[i].length; j++) //j = the values inside the 4 arrays{
             if (islandArray[i][j] === 1) {
                 oneCount++
-                console.log(oneCount)
+                console.log('the amount of ones is ', oneCount)
+                // console.log("islandArray is:", islandArray[i][j])
+                // console.log("islandArray1 is:", islandArray[i + 1][j])
             }
             else
                 console.log('Thats a zero')
         }
-    }
+    
 
-arrayReader(islandArray);
+// arrayReader(islandArray);
+
+//part B
+
+const arrayChecker = function (array) {
+    let islandCount = 0
+    for (let i = 0; i < array.length; i++)     {
+        for (j = 0; j < array[i].length; j++)  {
+            if (islandArray[i][j] === 1 && islandArray[i][j + 1] === 0 && islandArray[i][j - 1] ===0 && islandArray[i + 1][j] === 0 && islandArray[i - 1][j] === 0) {
+
+                islandCount++
+                console.log('island count:', islandCount)
+                // console.log("islandArray is:", islandArray[i][j])
+                // console.log("islandArray1 is:", islandArray[i + 1][j])
+            } else {
+                console.log('Thats a zero')
+            }
+        }
+    }
+}
+arrayChecker(islandArray);
